e6cac0662d366972f5b7e2ce3711f0faa40c81f7
Lester Ariel Mesa <lester_mesa@gbrnd.com>
2009-06-12 02:07:53 -0400

    import deals

---
+++ lib/ccda/Schema/Result/ImportDeals.pm
@@ -0,0 +1,89 @@
+package ccda::Schema::Result::ImportDeals;
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class';
+
+__PACKAGE__->load_components("InflateColumn::DateTime", "TimeStamp", "Core");
+__PACKAGE__->table("import_deals");
+__PACKAGE__->add_columns(
+  "id",
+  { data_type => "INT", default_value => undef, is_nullable => 0, size => 11 },
+  "md5",
+  {
+    data_type => "VARCHAR",
+    default_value => undef,
+    is_nullable => 0,
+    size => 128,
+  },
+  "date",
+  { data_type => "DATE", default_value => undef, is_nullable => 1, size => 10 },
+  "transaction_status",
+  {
+    data_type => "VARCHAR",
+    default_value => undef,
+    is_nullable => 1,
+    size => 128,
+  },
+  "callcenter_alias",
+  {
+    data_type => "VARCHAR",
+    default_value => undef,
+    is_nullable => 1,
+    size => 128,
+  },
+  "status",
+  {
+    data_type => "VARCHAR",
+    default_value => undef,
+    is_nullable => 1,
+    size => 128,
+  },
+  "reference",
+  {
+    data_type => "VARCHAR",
+    default_value => undef,
+    is_nullable => 1,
+    size => 128,
+  },
+  "lead_source",
+  {
+    data_type => "VARCHAR",
+    default_value => undef,
+    is_nullable => 1,
+    size => 128,
+  },
+  "customer_name",
+  {
+    data_type => "VARCHAR",
+    default_value => undef,
+    is_nullable => 1,
+    size => 128,
+  },
+  "charged_amount",
+  { data_type => "DECIMAL", default_value => undef, is_nullable => 1, size => 5 },
+  "card_type",
+  {
+    data_type => "VARCHAR",
+    default_value => undef,
+    is_nullable => 1,
+    size => 128,
+  },
+  "recording",
+  {
+    data_type => "VARCHAR",
+    default_value => undef,
+    is_nullable => 1,
+    size => 128,
+  },
+);
+__PACKAGE__->set_primary_key("id");
+
+
+# Created by DBIx::Class::Schema::Loader v0.04005 @ 2009-06-12 02:03:43
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:FEP6HfbEiSDViGZzCG9qEA
+
+
+# You can replace this text with custom content, and it will be preserved on regeneration
+1;
